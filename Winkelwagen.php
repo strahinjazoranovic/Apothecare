44a1c7a6bb072baf375f190cd77dca850fcdfabc
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2025-04-02 11:38:02 +0200

    UPDATE: Winkelwagen
    Hier is de begincode voor mijn winkelwagen

---
+++ Winkelwagen.php
@@ -0,0 +1,35 @@
+<?php
+//---------------------------------------------------------------------------------------------------//
+// Naam script      : Winkelwagen.php
+// Omschrijving     : Dit is de winkelwagen voor de website
+// Naam ontwikkelaar: Tejo Veldman
+// Project          : Apothecare
+// Datum            : start: 01-04-2025
+//---------------------------------------------------------------------------------------------------//
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Winkelwagen</title>
+    <link rel="shortcut icon" type="x-icon" href="icons/cart.svg">
+    <link rel="stylesheet" href="css/over.css">
+</head>
+<body>
+    <header>
+        <div class="logo">Apothe<span>care</span></div>
+        <nav>
+            <ul>
+                <li><a href="#">Producten</a></li>
+                <li><a href="#">Over</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="icons">
+            <a href="#" aria-label="Shopping Cart">🛒</a>
+            <a href="#" aria-label="User Account">👤</a>
+            <a href="#" aria-label="Search">🔍</a>
+        </div>
+    </header>
+</body>
+</html>
