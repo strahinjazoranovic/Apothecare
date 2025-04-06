6475cb6575169c223cac2fb27c7e76d464d76750
Tejo-TV <150041189+Tejo-TV@users.noreply.github.com>
2025-04-06 18:06:50 +0200

    UPDATE: persoonlijkere winkelwagen favicon
    de favicon aangepast van de winkelwagen

--- MySQL/apothecare.sql
+++ MySQL/apothecare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 06 apr 2025 om 15:02
+-- Gegenereerd op: 06 apr 2025 om 17:49
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -42,30 +42,28 @@ CREATE TABLE `producten` (
 --
 
 INSERT INTO `producten` (`ID`, `naam`, `beschrijving`, `prijs`, `image_url`, `pilsoort`, `datum_toegevoegd`) VALUES
-(1, 'Paracetamol', 'Pijnstillers die koorts verlagen en helpen bij het verlichten van pijn, zoals hoofdpijn en spierpijn.', 4.99, 'images/medicijn/paracetamol.png', 'Pijnstillers', '2025-04-05 11:57:44'),
-(2, 'Ibuprofen', 'Pijnstillers en ontstekingsremmers die helpen bij pijn, koorts en ontstekingen, zoals bij gewrichtspijn of spierpijn.', 5.49, 'images/medicijn/ibuprofen.png', 'Pijnstillers', '2025-04-05 11:57:44'),
-(3, 'Amoxicilline', 'Antibioticum gebruikt voor de behandeling van bacteriële infecties, zoals oor- en keelontstekingen.', 12.99, 'images/medicijn/amoxicilline.png', 'Antibiotica', '2025-04-05 11:57:44'),
-(4, 'Ciprofloxacine', 'Antibioticum effectief tegen bacteriële infecties zoals urineweginfecties en luchtweginfecties.', 14.49, 'images/medicijn/ciprofloxacine.png', 'Antibiotica', '2025-04-05 11:57:44'),
-(5, 'Cetirizine', 'Antihistaminicum dat helpt bij allergieën zoals hooikoorts, jeuk en huiduitslag.', 7.99, 'images/medicijn/cetirizine.png', 'Allergiemedicijnen', '2025-04-05 11:57:44'),
-(6, 'Fluticason', 'Neusspray die de symptomen van allergieën zoals verstopte neus en niezen verlicht.', 9.99, 'images/medicijn/fluticason.png', 'Allergiemedicijnen', '2025-04-05 11:57:44'),
-(7, 'Rennie', 'Antacidum dat zuurbranden en maagklachten verlicht door het neutraliseren van overtollig maagzuur.', 3.99, 'images/medicijn/rennie.png', 'Maag- en darmmedicijnen', '2025-04-05 11:57:44'),
-(8, 'Bisacodyl', 'Laxeermiddel dat helpt bij de behandeling van constipatie door de darmen te stimuleren.', 4.49, 'images/medicijn/bisacodyl.png', 'Maag- en darmmedicijnen', '2025-04-05 11:57:44'),
-(9, 'Enalapril', 'ACE-remmer die wordt gebruikt voor de behandeling van hoge bloeddruk en bepaalde hartproblemen.', 10.99, 'images/medicijn/enalapril.png', 'Hoge bloeddruk medicijnen', '2025-04-05 11:57:44'),
-(10, 'Furosemide', 'Diureticum (plastablet) dat wordt gebruikt om vochtophoping te verminderen en de bloeddruk te verlagen.', 11.49, 'images/medicijn/furosemide.png', 'Hoge bloeddruk medicijnen', '2025-04-05 11:57:44'),
-(11, 'Hydrocortison', 'Zalf die jeuk en ontstekingen op de huid vermindert, bijvoorbeeld bij eczeem of allergieën.', 6.99, 'images/medicijn/hydrocortison.png', 'Huidmedicijnen', '2025-04-05 11:57:44'),
-(12, 'Clotrimazol', 'Antischimmelcrème die wordt gebruikt om schimmelinfecties zoals athletes foot en ringworm te behandelen.', 8.49, 'images/medicijn/clotrimazol.png', 'Huidmedicijnen', '2025-04-05 11:57:44'),
-(13, 'Dextromethorfan', 'Hoestdrank die helpt bij het onderdrukken van droge hoest door de hoestreflex te dempen.', 5.99, 'images/medicijn/dextromethorfan.png', 'Hoest- en verkoudheidsmiddelen', '2025-04-05 11:57:44'),
-(14, 'Xylometazoline', 'Decongestivum neusspray die helpt bij het verlichten van een verstopte neus door de bloedvaten in de neus te vernauwen.', 4.99, 'images/medicijn/xylometazoline.png', 'Hoest- en verkoudheidsmiddelen', '2025-04-05 11:57:44'),
-(15, 'Insuline', 'Insuline-injecties die worden gebruikt voor de behandeling van diabetes door de bloedsuikerspiegel te reguleren.', 25.99, 'images/medicijn/insuline.png', 'Diabetes medicijnen', '2025-04-05 11:57:44'),
-(16, 'Metformine', 'Medicijn dat de bloedsuikerspiegel verlaagt en wordt gebruikt bij de behandeling van type 2 diabetes.', 13.99, 'images/medicijn/metformine.png', 'Diabetes medicijnen', '2025-04-05 11:57:44'),
-(17, 'Multivitaminen', 'Supplementen die essentiële vitamines en mineralen leveren ter ondersteuning van de algemene gezondheid en het welzijn.', 8.99, 'images/medicijn/multivitaminen.png', 'Vitamine- en mineralensupplementen', '2025-04-05 11:57:44'),
-(18, 'Vitamine D', 'Vitamine D-supplement dat helpt bij de opname van calcium en het versterken van botten en het immuunsysteem.', 6.49, 'images/medicijn/vitamine_d.png', 'Vitamine- en mineralensupplementen', '2025-04-05 11:57:44'),
-(19, 'Kunsttranen', 'Oogdruppels die helpen bij het verlichten van droge ogen, bijvoorbeeld door computergebruik of weersomstandigheden.', 5.49, 'images/medicijn/kunsttranen.png', 'Oogdruppels', '2025-04-05 11:57:44'),
-(20, 'De pil', 'Anticonceptiepil die helpt bij het voorkomen van zwangerschap door hormonen die de ovulatie blokkeren.', 15.99, 'images/medicijn/de_pil.png', 'Anticonceptiemiddelen', '2025-04-05 11:57:44'),
-(21, 'Melatonine', 'Slaapmiddel dat helpt bij het reguleren van het slaap-waakritme, vaak gebruikt bij slaapproblemen of jetlag.', 9.49, 'images/medicijn/melatonine.png', 'Slaap- en kalmeringsmiddelen', '2025-04-05 11:57:44'),
-(22, 'Griepvaccin', 'Vaccin dat bescherming biedt tegen de griep en helpt om de verspreiding van het virus te voorkomen.', 20.00, 'images/medicijn/griepvaccin.png', 'Vaccins', '2025-04-05 11:57:44'),
-(23, 'NuvaRing', 'Vaginale anticonceptiering die hormonen afgeeft om zwangerschap te voorkomen, gebruikt voor een maandelijkse cyclus.', 18.99, 'images/medicijn/nuvaring.png', 'Anticonceptiepillen', '2025-04-05 11:57:44'),
-(24, 'Pleisters', 'Eerste hulp pleisters die worden gebruikt om kleine wonden af te dekken en te beschermen tegen infecties.', 2.99, 'images/medicijn/pleisters.png', '', '2025-04-05 11:57:44');
+(1, 'Paracetamol', 'Pijnstillers die koorts verlagen en helpen bij het verlichten van pijn, zoals hoofdpijn en spierpijn.', 4.99, 'paracetamol.png', 'Pijnstillers', '2025-04-05 11:57:44'),
+(2, 'Ibuprofen', 'Pijnstillers en ontstekingsremmers die helpen bij pijn, koorts en ontstekingen, zoals bij gewrichtspijn of spierpijn.', 5.49, 'ibuprofen.png', 'Pijnstillers', '2025-04-05 11:57:44'),
+(3, 'Amoxicilline', 'Antibioticum gebruikt voor de behandeling van bacteriële infecties, zoals oor- en keelontstekingen.', 12.99, 'amoxicilline.png', 'Antibiotica', '2025-04-05 11:57:44'),
+(4, 'Ciprofloxacine', 'Antibioticum effectief tegen bacteriële infecties zoals urineweginfecties en luchtweginfecties.', 14.49, 'ciprofloxacine.png', 'Antibiotica', '2025-04-05 11:57:44'),
+(5, 'Cetirizine', 'Antihistaminicum dat helpt bij allergieën zoals hooikoorts, jeuk en huiduitslag.', 7.99, 'cetirizine.png', 'Allergiemedicijnen', '2025-04-05 11:57:44'),
+(6, 'Fluticason', 'Neusspray die de symptomen van allergieën zoals verstopte neus en niezen verlicht.', 9.99, 'fluticason.png', 'Allergiemedicijnen', '2025-04-05 11:57:44'),
+(7, 'Rennie', 'Antacidum dat zuurbranden en maagklachten verlicht door het neutraliseren van overtollig maagzuur.', 3.99, 'rennie.png', 'Maag- en darmmedicijnen', '2025-04-05 11:57:44'),
+(8, 'Bisacodyl', 'Laxeermiddel dat helpt bij de behandeling van constipatie door de darmen te stimuleren.', 4.49, 'bisacodyl.png', 'Maag- en darmmedicijnen', '2025-04-05 11:57:44'),
+(9, 'Enalapril', 'ACE-remmer die wordt gebruikt voor de behandeling van hoge bloeddruk en bepaalde hartproblemen.', 10.99, 'enalapril.png', 'Hoge bloeddruk medicijnen', '2025-04-05 11:57:44'),
+(10, 'Furosemide', 'Diureticum (plastablet) dat wordt gebruikt om vochtophoping te verminderen en de bloeddruk te verlagen.', 11.49, 'furosemide.png', 'Hoge bloeddruk medicijnen', '2025-04-05 11:57:44'),
+(11, 'Hydrocortison', 'Zalf die jeuk en ontstekingen op de huid vermindert, bijvoorbeeld bij eczeem of allergieën.', 6.99, 'hydrocortison.png', 'Huidmedicijnen', '2025-04-05 11:57:44'),
+(12, 'Clotrimazol', 'Antischimmelcrème die wordt gebruikt om schimmelinfecties zoals athletes foot en ringworm te behandelen.', 8.49, 'clotrimazol.png', 'Huidmedicijnen', '2025-04-05 11:57:44'),
+(13, 'Dextromethorfan', 'Hoestdrank die helpt bij het onderdrukken van droge hoest door de hoestreflex te dempen.', 5.99, 'dextromethorfan.png', 'Hoest- en verkoudheidsmiddelen', '2025-04-05 11:57:44'),
+(14, 'Xylometazoline', 'Decongestivum neusspray die helpt bij het verlichten van een verstopte neus door de bloedvaten in de neus te vernauwen.', 4.99, 'xylometazoline.png', 'Hoest- en verkoudheidsmiddelen', '2025-04-05 11:57:44'),
+(15, 'Insuline', 'Insuline-injecties die worden gebruikt voor de behandeling van diabetes door de bloedsuikerspiegel te reguleren.', 25.99, 'insuline.png', 'Diabetes medicijnen', '2025-04-05 11:57:44'),
+(16, 'Metformine', 'Medicijn dat de bloedsuikerspiegel verlaagt en wordt gebruikt bij de behandeling van type 2 diabetes.', 13.99, 'metformine.png', 'Diabetes medicijnen', '2025-04-05 11:57:44'),
+(17, 'Multivitaminen', 'Supplementen die essentiële vitamines en mineralen leveren ter ondersteuning van de algemene gezondheid en het welzijn.', 8.99, 'multivitaminen.png', 'Vitamine- en mineralensupplementen', '2025-04-05 11:57:44'),
+(18, 'Vitamine D', 'Vitamine D-supplement dat helpt bij de opname van calcium en het versterken van botten en het immuunsysteem.', 6.49, 'vitamine_d.png', 'Vitamine- en mineralensupplementen', '2025-04-05 11:57:44'),
+(19, 'Kunsttranen', 'Oogdruppels die helpen bij het verlichten van droge ogen, bijvoorbeeld door computergebruik of weersomstandigheden.', 5.49, 'kunsttranen.png', 'Oogdruppels', '2025-04-05 11:57:44'),
+(20, 'De pil', 'Anticonceptiepil die helpt bij het voorkomen van zwangerschap door hormonen die de ovulatie blokkeren.', 15.99, 'de_pil.png', 'Anticonceptiemiddelen', '2025-04-05 11:57:44'),
+(21, 'Melatonine', 'Slaapmiddel dat helpt bij het reguleren van het slaap-waakritme, vaak gebruikt bij slaapproblemen of jetlag.', 9.49, 'melatonine.png', 'Slaap- en kalmeringsmiddelen', '2025-04-05 11:57:44'),
+(23, 'NuvaRing', 'Vaginale anticonceptiering die hormonen afgeeft om zwangerschap te voorkomen, gebruikt voor een maandelijkse cyclus.', 18.99, 'nuvaring.png', 'Anticonceptiepillen', '2025-04-05 11:57:44');
 
 -- --------------------------------------------------------
 
--- php/winkelwagen.php
+++ php/winkelwagen.php
@@ -16,7 +16,7 @@
     <title>Apothecare - Winkelwagen</title>
     <link rel="stylesheet" href="../css/main.css" />
     <link rel="stylesheet" href="../css/winkelwagen.css" />
-    <link rel="shortcut icon" type="x-icon" href="../images/icons/cart.svg" />
+    <link rel="shortcut icon" type="x-icon" href="../images/icons/cart-favicon.png" />
     <!-- Dit is voor de font-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
